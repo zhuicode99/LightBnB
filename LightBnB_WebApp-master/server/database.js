7206a7a5154ec1c004cc073b385db87ad3990a08
zhuicode99 <enlin291814833@hotmail.com>
2022-09-23 05:23:27 +0000

    update getallreservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -78,9 +78,29 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = `
+SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id 
+WHERE reservations.guest_id = $1
+GROUP BY reservations.id, properties.id
+ORDER BY reservations.start_date
+LIMIT $2;`
+const values = [guest_id, limit]
+return pool.query(query, values).then((res) => {
+  if (res.rows.length === 0) {
+    return null;
+  }
+  return res.rows;
+});
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
+
+
+
+
 
 /// Properties
 
