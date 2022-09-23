9d68e27304ec0435676033dc6210cea00f819a8a
zhuicode99 <enlin291814833@hotmail.com>
2022-09-23 01:06:24 +0000

    replace getallproperties function with query and promise

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,17 @@
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+// the following assumes that you named your connection variable `pool`
+// pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
+
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+
 
 /// Users
 
@@ -66,13 +78,17 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
+ const getAllProperties = (options, limit = 10) => {
+  return pool//return the appropriate pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;//Return the appropriate value from our getAllProperties function.
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 exports.getAllProperties = getAllProperties;
 
 
